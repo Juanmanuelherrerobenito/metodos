b559bd137482ddab8e6f72320250eff6a9f72600
Juanmanuelherrerobenito <91980407+Juanmanuelherrerobenito@users.noreply.github.com>
2022-02-10 08:54:36 +0100

    Add files via upload

---
+++ javascrit/java.js
@@ -0,0 +1,8 @@
+let texto ="En un lugar de la mancha"; 
+//document.getElementById("demo").innerHTML = texto;
+var a="LONGITUD: ";
+alert(a+texto.length);
+let str ="En un lugar de la mancha"; 
+alert(str.slice(18,24));
+let reemplazo=texto.replace("mancha","antártida");
+alert(reemplazo);
